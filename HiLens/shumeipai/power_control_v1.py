78c777e82993b5aa61d14a97e7ea0c928f7861d5
clevercong <quizkid@163.com>
2020-10-25 23:06:12 +0800

    Update power_control_v1.py

--- HiLens/shumeipai/power_control_v1.py
+++ HiLens/shumeipai/power_control_v1.py
@@ -1,27 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
-channel = 24  # 管脚40，参阅树莓派引脚图，物理引脚40对应的BCM编码为21
 print("PC ON: PC-ON")
 print("Exit: Q and q")
 
 while True:
     user_choice = input("Choice:")
-    if user_choice == "1":
+    if user_choice == "PC-ON":
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(channel, GPIO.OUT)
-        GPIO.output(channel, GPIO.LOW)
-        print("3")
-        time.sleep(1.0)
-        print("2")
-        time.sleep(1.0)
-        print("1")
-        time.sleep(1.0)
-        GPIO.cleanup()
-    elif user_choice == "2":
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(channel, GPIO.OUT)
-        GPIO.output(channel, GPIO.HIGH)
+        GPIO.setup(25, GPIO.OUT)
+        GPIO.output(25, GPIO.LOW)
         print("3")
         time.sleep(1.0)
         print("2")
