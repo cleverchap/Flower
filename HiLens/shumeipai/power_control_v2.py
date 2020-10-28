36772a510eecd1a377dc225bfbc854308ecebca9
clevercong <quizkid@163.com>
2020-10-28 08:39:18 +0800

    Update power_control_v2.py

--- HiLens/shumeipai/power_control_v2.py
+++ HiLens/shumeipai/power_control_v2.py
@@ -3,18 +3,20 @@ import time
 
 from HiLens.shumeipai.settings import power_channel
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(power_channel, GPIO.OUT)
-
 
 def blink(times):
-    GPIO.output(power_channel, GPIO.LOW)
+    # 设置channel=8的接口的编号方式是输出，默认是低电平
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(power_channel, GPIO.OUT, initial=GPIO.LOW)
+    # GPIO.output(power_channel, GPIO.LOW)
     while times > 0:
+        GPIO.output(power_channel, GPIO.HIGH)
         time.sleep(1)
         print(times)
         times -= 1
-    GPIO.output(power_channel, GPIO.HIGH)
+    GPIO.output(power_channel, GPIO.LOW)
     GPIO.cleanup()
+    time.sleep(2)
 
 
 if __name__ == '__main__':
