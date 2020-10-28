3794dd5380ca7d90968e74317e1f0ecaf40217c8
clevercong <quizkid@163.com>
2020-10-28 21:09:30 +0800

    Update client.py

--- HiLens/shumeipai/client.py
+++ HiLens/shumeipai/client.py
@@ -3,6 +3,7 @@ from time import *
 
 from HiLens.shumeipai.bme280 import get_temp_pressure_humidity_from_sensor
 from HiLens.shumeipai.sensor import get_result_from_sensor
+from HiLens.shumeipai.settings import sensor_channel
 
 HOST = '192.168.43.236'  # or 'localhost'
 PORT = 21567
@@ -15,7 +16,7 @@ while True:
     sleep(10)
     # data1 = input('>')
     t1, t2, temperature, pressure, humidity = get_temp_pressure_humidity_from_sensor()
-    dry_or_humid = get_result_from_sensor()
+    dry_or_humid = get_result_from_sensor(sensor_channel)
     # data1 = ("Temperature : %f C, Pressure : %f hPa, Humidity : %f RH" % (temperature, pressure, humidity))
     # data = str(data)
     data1 = ("%f,%f,%f,%s" % (temperature, pressure, humidity, dry_or_humid))
